4477c306fa4f72c5191362cf79a164d00d1045ad
kdbaylon <kdbaylon@up.edu.ph>
2019-11-26 17:47:23 +0800

    main menu

---
+++ mm.py
@@ -0,0 +1,19 @@
+def mainMenu():
+	print("======================================================")
+	print(" ooo   oooo   oooo    oooooo   ooooo    ooooo  ooooo")
+	print("o oo  o  oo      oo   oo    o oo    o  ooo    ooo")	
+	print("  oo    oo    ooo     oooooo  ooooooo   oooo   oooo")
+	print("  oo   oo        oo   oo      oo    o     ooo    ooo")	
+	print("ooooo oooooo  oooo    oo      oo    o  ooooo  ooooo")
+	print("======================================================")
+	print("[1] Enter")
+	print("[0] Exit")
+def createUser():
+	username=input("Enter username: ")
+def game():
+	deck=[]
+	cardToPass=input("Enter card name: ")
+	print("[1] PASS")
+	print("[2] DOWN")
+mainMenu()
+
